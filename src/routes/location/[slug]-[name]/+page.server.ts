ee5737f7cd6b38e6ff92fb8b31349d6faa2b2197
welsea <welsea.42@gmail.com>
2025-06-08 16:48:53 +0200

    debug console.log

--- src/routes/location/[slug]-[name]/+page.server.ts
+++ src/routes/location/[slug]-[name]/+page.server.ts
@@ -1,42 +1,50 @@
-import * as db from '$lib/server/db';
-import * as userDB from '$lib/server/user';
-import { fail } from '@sveltejs/kit';
+import * as db from "$lib/server/db";
+import * as userDB from "$lib/server/user";
+import { fail } from "@sveltejs/kit";
 
 export async function load({ params, parent }) {
 	const layoutData = await parent();
-	console.log(params.slug)
-	console.log(layoutData.locations)
-	console.log(layoutData.locations[0].id)
-	const currentLoc = layoutData.locations.filter((e) => e.id === params.slug)[0];
-	console.log(currentLoc)
+	console.log(params.slug);
+	console.log(layoutData.locations);
+	console.log(layoutData.locations[0].id == params.slug);
+	console.log(layoutData.locations[0].id === params.slug);
+	const currentLoc = layoutData.locations.filter(
+		(e) => e.id == params.slug
+	)[0];
+	console.log(layoutData.locations.filter(
+		(e) => e.id == params.slug
+	))
+	console.log(currentLoc);
 	const data = await db.getList(params.slug);
-	console.log(data)
+	console.log(data);
 	const username = await userDB.getUserName(currentLoc.last_updated_by);
 	const categories = await db.getCategory();
 	return {
 		location: {
 			id: params.slug,
 			name: params.name,
-			last_updated: new Date(currentLoc.last_updated).toLocaleString('no-NO'),
-			last_updated_by: username
+			last_updated: new Date(currentLoc.last_updated).toLocaleString(
+				"no-NO"
+			),
+			last_updated_by: username,
 		},
 		categories,
 		exists: data.exist,
-		needs: data.need
+		needs: data.need,
 	};
 }
 
 export const actions = {
 	edit: async ({ request }) => {
 		const data = await request.formData();
-		const actions = JSON.parse(data.get('events')?.toString() || '[]');
-		const location = data.get('location_id');
-		const user = data.get('user_id');
+		const actions = JSON.parse(data.get("events")?.toString() || "[]");
+		const location = data.get("location_id");
+		const user = data.get("user_id");
 		try {
 			for (const action of actions) {
 				const item = action.item;
 				switch (action.action) {
-					case 'ADD':
+					case "ADD":
 						await db.addItem(
 							action.target,
 							location,
@@ -45,10 +53,14 @@ export const actions = {
 							item.quantity
 						);
 						break;
-					case 'DELETE':
-						await db.deleteItem(action.target, location, item.item_id);
+					case "DELETE":
+						await db.deleteItem(
+							action.target,
+							location,
+							item.item_id
+						);
 						break;
-					case 'UPDATE':
+					case "UPDATE":
 						await db.updateItem(
 							action.target,
 							location,
@@ -62,16 +74,16 @@ export const actions = {
 			await db.updateLocation(Number(user), Number(location));
 		} catch (error: any) {
 			return fail(422, {
-				error: error.message
+				error: error.message,
 			});
 		}
 	},
 	add_category: async ({ request }) => {
 		try {
 			const data = await request.formData();
-			await db.addCategory(data.get('name'));
+			await db.addCategory(data.get("name"));
 		} catch (error) {
 			console.log(error);
 		}
-	}
+	},
 };
